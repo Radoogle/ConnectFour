16653b4956d6aa3eeae2bbec5f5f740bad125084
Radu Harangus <Radu.Harangus@gmail.com>
2020-08-19 15:49:32 +0300

    fixed overlap players

--- app.js
+++ app.js
@@ -5,29 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const result = document.querySelector('#result');
     const displayCurrentPlayer = document.querySelector('#current-player');
     let currentPlayer = 1;
+    const info = document.querySelector('.info');
 
     for (var i = 0, len = squares.length; i < len; i++) 
         (function(index) {
             //add an onclick to each square in your grid
-            squares[i].onclick = function() {
+            squares[index].onclick = function() {
                 //if the square below your current square is taken, you can go ontop of it
-                if(squares[index + 7].classList.contains('taken')) {
-                    if (currentPlayer === 1) {
-                        squares[index].classList.add('taken');
-                        squares[index].classList.add('player-one');
-                        //change current player
-                        currentPlayer = 2;
-                        displayCurrentPlayer.innerHTML = currentPlayer;
-                    } else if (currentPlayer === 2) {
-                        squares[index].classList.add('taken');
-                        squares[index].classList.add('player-two');
-                        //change current Player
-                        currentPlayer = 1;
-                        displayCurrentPlayer.innerHTML = currentPlayer;
-                    }
+                if(!squares[index].classList.contains('taken'))
+                    if(squares[index + 7].classList.contains('taken')) {
+                        if (currentPlayer === 1) {
+                            squares[index].classList.add('taken');
+                            squares[index].classList.add('player-one');
+                            //change current player
+                            currentPlayer = 2;
+                            displayCurrentPlayer.innerHTML = currentPlayer;
+                        } else if (currentPlayer === 2) {
+                            squares[index].classList.add('taken');
+                            squares[index].classList.add('player-two');
+                            //change current Player
+                            currentPlayer = 1;
+                            displayCurrentPlayer.innerHTML = currentPlayer;
+                        }
                     //if the square below your current square is not taken, you can't go there
-
-                } else alert('cant go there');
+                    } else alert('cant go there');
             }
         })(i);
 
